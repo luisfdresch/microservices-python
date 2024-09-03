d2e1f2090477122a71548860a9f66c0f08515661
Luis Fernando Dresch <66801762+luisfdresch@users.noreply.github.com>
2024-09-03 19:20:42 -0300

    Edit references from gmail to mailhog

--- src/notification/send/email.py
+++ src/notification/send/email.py
@@ -5,27 +5,25 @@ import json
 
 
 def notification(message):
-    # try:
-    message = json.loads(message)
-    mp3_fid = message["mp3_fid"]
-    sender_address = os.environ.get("GMAIL_ADDRESS")
-    sender_password = os.environ.get("GMAIL_PASSWORD")
-    receiver_address = message["username"]
+    try:
+        message = json.loads(message)
+        mp3_fid = message["mp3_fid"]
+        sender_address = os.environ.get("MAIL_ADDRESS")
+        smtp_server = os.environ.get("SMTP_SERVER")
+        smtp_port = int(os.environ.get("SMTP_PORT"))
+        receiver_address = message["username"]
 
-    msg = EmailMessage()
-    msg.set_content(f"mp3 file_id: {mp3_fid} is now ready!")
-    msg["Subject"] = "MP3 Download"
-    msg["From"] = sender_address
-    msg["To"] = receiver_address
+        msg = EmailMessage()
+        msg.set_content(f"mp3 file_id: {mp3_fid} is now ready!")
+        msg["Subject"] = "mp3 download"
+        msg["From"] = sender_address
+        msg["To"] = receiver_address
 
-    session = smtplib.SMTP("smtp.gmail.com", 587)
-    session.starttls()
-    session.login(sender_address, sender_password)
-    session.send_message(msg, sender_address, receiver_address)
-    session.quit()
-    print("Mail Sent")
-
-
-# except Exception as err:
-# print(err)
-# return err
+        server = smtplib.SMTP(smtp_server, smtp_port)
+        server.send_message(msg)
+        print("Mail sent")
+    except Exception as err:
+        print(err)
+        return err
+    finally:
+        server.quit()
